2eb7f3862339723ad951d461b34d3aabf8982950
Thomas Dimnet <Thomas.dimnet@gmail.com>
2017-03-09 12:03:54 +0100

    Start creating the searchForAName function and add new const for the array of persons

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+// Selecting the elements already here inside the DOM
+const studentsArray           = document.querySelectorAll('.student-details');
+
+
 // Creating DOM Elements
 const studentSearchContainer  = document.createElement('div');
 const searchInput             = document.createElement('input');
@@ -15,3 +19,23 @@ studentSearchContainer.appendChild(searchInput);
 studentSearchContainer.appendChild(searchButton);
 // Then adding them to the DOM
 document.querySelector('.page-header').appendChild(studentSearchContainer);
+
+
+// The search function
+function searchForAName() {
+  // Asigning the search input value
+  const searchText = searchInput.value;
+
+  // If the search input is not filled in, show error
+  if (searchText.length === 0) {
+    alert('Please fill in the form..');
+  // Else search for the name
+  } else {
+    console.log(searchText);
+  }
+
+} // End: searchForAName function
+
+
+// Create the event handlers
+searchButton.onclick = searchForAName;
